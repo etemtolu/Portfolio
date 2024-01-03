7c6e4fa44a331a8167f18a323312222bbaecbbac
etemtolu <42868963+etemtolu@users.noreply.github.com>
2024-01-03 21:57:30 +0300

    first commit

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Projects } from "../constants";
+import ProjectCard from "../components/ProjectCard";
 
 const Page = () => {
   return (
@@ -7,7 +9,14 @@ const Page = () => {
       className="w-screen h-screen flex items-center justify-center bg-center bg-cover"
     >
       <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]">
-        
+        {Projects.map((project , index) => (
+          <ProjectCard 
+          key={index}
+          title={project.title}
+          text={project.text}
+          image={project.src}
+          />
+        ))}
       </div>
     </div>
   );
