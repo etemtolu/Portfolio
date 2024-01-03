b0a231eb1d00eec886bc2a302f0183e98b1a13d7
etemtolu <42868963+etemtolu@users.noreply.github.com>
2024-01-03 17:36:17 +0300

    first commit

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Page = () => {
   return (
-    <div>My Project</div>
-  )
-}
+    <div
+      style={{ backgroundImage: "url(/mountains.jpg)" }}
+      className="w-screen h-screen flex items-center justify-center bg-center bg-cover"
+    >
+      <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]">
+        
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
